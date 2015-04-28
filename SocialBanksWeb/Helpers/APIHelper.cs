5fc1519c619e15dae7b65aaf3c6c72d6dcd63c1c
Fabricio Matos <fabriciovargasmatos@gmail.com>
2015-04-28 00:26:41 -0400

    new class APIHelper

---
+++ SocialBanksWeb/Helpers/APIHelper.cs
@@ -0,0 +1,41 @@
+﻿using Parse;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SocialBanksWeb.Helpers
+{
+    public class APIHelper
+    {
+        private static APIHelper instance;
+
+        //TODO: Move to web.config and generate new keys (don't publish our keys!)
+        private const string ApplicationId_DEV = "bCOd9IKjrpxCPGYQfyagabirn7pYFjYTvJqkq1x1";
+        private const string DotnetKey_DEV = "GYMOAhUQ55yYAuEehlecpipu90RFeaPSPn3zcFZ6";
+
+        static APIHelper()
+        {
+            ParseClient.Initialize(ApplicationId_DEV, DotnetKey_DEV);
+        }
+
+        public static APIHelper Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new APIHelper();
+                }
+                return instance;
+            }
+        }
+
+        public string Hello()
+        {
+            return "Hello World!";
+        }
+
+        //public create_new_currency() ...
+    }
+}
---
+++ UnitTests/APIHelperTest.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SocialBanksWeb.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class APIHelperTest
+    {
+        [TestMethod]
+        public void InstanceIsntNull()
+        {
+            var instance = APIHelper.Instance;
+            Assert.IsNotNull(instance);
+        }
+
+        [TestMethod]
+        public void HelloWorld()
+        {
+            Assert.AreEqual("Hello World!", APIHelper.Instance.Hello());
+        }
+    }
+}
