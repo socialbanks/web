18cae34be9ebbf9e8e7514b8100bff99a3d22a36
Osias Maia <osiasjota@gmail.com>
2015-05-13 22:59:42 -0300

    Spliting SocialBanksWeb.csproj folder

---
+++ SocialBanksWeb/Content/Scripts/CreateSocialBank.js
@@ -0,0 +1,12 @@
+﻿function CreateSocialBank() {
+    var self = this;
+
+    this.Ready = function () {
+        $('#btn-create').click(self.Click_create);
+    }
+
+    this.Click_create = function () {
+        alert(0);
+    }
+
+}
